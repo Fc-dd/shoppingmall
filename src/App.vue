547c885175e1554077e6c582983f5e936f85a42d
fc <2394651228@qq.com>
2022-11-07 16:17:18 +0800

    配置 service

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <div>
+    <button @click="get_fruits_Info">获取所有水果信息</button>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
+table,
+td,
+th {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
 
+table {
+  width: 500px;
+  text-align: center;
+}
+
+tr {
+  height: 35px;
+}
 </style>
---
+++ src/uitls/request.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+// 创建实例
+const service = axios.create({
+  baseURL: 'http://localhost:3004/',
+  timeout: 5000
+})
+
+// 添加请求拦截器
+service.interceptors.request.use( 
+  config => {
+    // 在发送请求之前做什么
+    return config;
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+// 添加响应拦截器
+service.interceptors.response.use(
+  response => {
+    // 2xx 范围内的状态码都会触发该函数
+    // 对响应数据做点什么
+    return response
+  },
+  error => {
+    // 超出 2xx 范围的状态码都会触发该函数
+    // 对响应错误做点什么
+
+    return Promise.reject(error)
+  }
+)
